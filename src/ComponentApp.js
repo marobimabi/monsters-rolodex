7c0d86df3173f60cd1aa2669be9a5e9fcdd452fb
mahamat ali youssouf <56170343+marobimabi@users.noreply.github.com>
2022-06-24 14:25:30 +0300

    Adding new Component

---
+++ src/ComponentApp.js
@@ -0,0 +1,61 @@
+import { Component } from 'react';
+import CartList from './components/card-List/card-list-component';
+import SearchBox from './components/searchBox/search-box.component';
+
+
+
+import './App.css';
+
+class ComponentApp extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+     
+      Cars: [],
+      searchField: ''
+    } 
+  };
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then((Response) => Response.json()).then((mycar)=>
+    this.setState(
+      ()=> {
+        return {Cars : mycar};
+      }
+    )
+    )
+  }
+  
+  OnsearchChange = (event) =>{ 
+         
+    const searchField = event.target.value.toLocaleLowerCase();
+    
+    this.setState(
+     () => {
+       return {searchField};
+     }
+    )
+   }
+  render(){
+    const  {OnsearchChange} = this;
+    const {searchField, Cars} = this.state;
+
+    const filterredMonsters = Cars.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+
+     });
+
+    return (
+      <div className="App">
+       <h1 className='app-title'>Pobohash Monsters!!!</h1>
+       <SearchBox onSearchHandler={OnsearchChange} placeholder='Search Monsters' />
+       <CartList monsters = {filterredMonsters }/> 
+       
+      </div>
+    );
+  }
+}
+
+export default ComponentApp;
